b6c5ec65b18506968b7edf5d28b231f009a19064
goslv <frenzengamer@gmail.com>
2026-02-09 14:58:50 -0300

    Actualización del settings: DB Configurada

--- syscep/settings.py
+++ syscep/settings.py
@@ -66,10 +66,12 @@ WSGI_APPLICATION = 'syscep.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': config('DB_NAME'),
+    'USER': config('DB_USER'),
+    'PASSWORD': config('DB_PASSWORD'),
+    'HOST': 'localhost',
+    'PORT': '5432',
 }
 
 # Password validation
