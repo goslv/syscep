eb27bc4fb17bb089bd6606efa698eb5a8fd66292
goslv <frenzengamer@gmail.com>
2026-03-14 20:45:25 -0300

    Admin-acces mejorado

--- syscep/urls.py
+++ syscep/urls.py
@@ -2,11 +2,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+from django.contrib import messages
 
 # Personalizar títulos del admin
 admin.site.site_header = "CEP - Sistema Administrativo"
 admin.site.site_title = "CEP Admin"
 admin.site.index_title = "Panel de Administración"
+
+def admin_access_check(view):
+    def wrapper(request, *args, **kwargs):
+        if request.path.startswith('/admin/') and request.user.is_authenticated:
+            if not request.user.is_superuser:
+                messages.error(request, "Acceso denegado al panel de administración.")
+                return redirect('dashboard')
+        return view(request, *args, **kwargs)
+    return wrapper
 
 urlpatterns = [
     path('admin/', admin.site.urls),
